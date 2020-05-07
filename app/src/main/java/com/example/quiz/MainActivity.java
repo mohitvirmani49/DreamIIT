23c43bd0b738d50555cde711d4fb1d58134654fd
mohit_virmani49 <contact.mohitvirmani@gmail.com>
2020-05-07 23:48:18 +0530

    Quiz App

--- app/src/main/java/com/example/quiz/MainActivity.java
+++ app/src/main/java/com/example/quiz/MainActivity.java
@@ -9,12 +9,18 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
     Button b1;
+    Button phy,chem,maths,full;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         b1 = (Button)findViewById(R.id.btn);
+        phy = (Button)findViewById(R.id.physics);
+        chem = (Button)findViewById(R.id.chemistry);
+        maths = (Button)findViewById(R.id.maths);
+        full = (Button) findViewById(R.id.full_paper);
+
         b1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -22,9 +28,23 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+
+        maths.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                start_maths();
+            }
+        });
+
+
     }
     private void startQuiz(){
         Intent intent = new Intent(this,Main2Activity.class);
         startActivity(intent);
+    }
+    private void start_maths(){
+        Intent intent = new Intent(this,Main4Activity.class);
+        startActivity(intent);
+
     }
 }
