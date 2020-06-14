9630b5d9a455e95d60eee4b7fb43c8e14b51cc56
mohit_virmani49 <contact.mohitvirmani@gmail.com>
2020-06-15 03:22:28 +0530

    Layouts Update- v5.5

--- app/src/main/java/com/example/quiz/Main13Activity.java
+++ app/src/main/java/com/example/quiz/Main13Activity.java
@@ -4,27 +4,44 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.squareup.picasso.Picasso;
 
 public class Main13Activity extends AppCompatActivity {
-    EditText editText;
+    TextView name1, name2, ranks;
     ImageView imageView;
+    ImageButton back;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main13);
-        editText = (EditText)findViewById(R.id.name);
-        imageView = (ImageView)findViewById(R.id.profile_pic);
+        name1 = (TextView) findViewById(R.id.profile_other_user_pic);
+        name2 = (TextView) findViewById(R.id.p_user_nm);
+        imageView = (ImageView) findViewById(R.id.profile_other_user_pic);
+        back = (ImageButton) findViewById(R.id.back_button);
 
         GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(getApplicationContext());
         String name = account.getDisplayName();
-        editText.setText(name);
+        name1.setText(name);
+        name2.setText(name);
         Picasso.get().load(account.getPhotoUrl()).into(imageView);
+
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Main13Activity.this, Main14Activity.class);
+                startActivity(intent);
+            }
+        });
+
     }
+
 }
--- app/src/main/java/com/example/quiz/Main14Activity.java
+++ app/src/main/java/com/example/quiz/Main14Activity.java
@@ -1,19 +1,27 @@
 package com.example.quiz;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
+import androidx.fragment.app.Fragment;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.PopupMenu;
 import android.widget.RelativeLayout;
 
+import com.google.android.material.bottomnavigation.BottomNavigationMenu;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 public class Main14Activity extends AppCompatActivity {
     CardView cardView_phy, cardView_chem, cardView_maths, cardView_fulltest;
     RelativeLayout doubt;
+    BottomNavigationView menu;
+    private int menuId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +32,33 @@ public class Main14Activity extends AppCompatActivity {
         cardView_maths = (CardView) findViewById(R.id.mathematics);
         cardView_fulltest = (CardView) findViewById(R.id.full_test_papers);
         doubt = (RelativeLayout) findViewById(R.id.doubts);
+        menu = (BottomNavigationView) findViewById(R.id.bottomNavigation);
+
+        menu.setSelectedItemId(R.id.home);
+        menu.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId()) {
+                    case R.id.notification9:
+                        startActivity(new Intent(getApplicationContext(), ImagesActivity.class));
+                        overridePendingTransition(0, 0);
+                        return true;
+
+                    case R.id.profile9:
+                        startActivity(new Intent(getApplicationContext(), Main13Activity.class));
+                        overridePendingTransition(0, 0);
+                        return true;
+
+                    case R.id.home:
+                        startActivity(new Intent(getApplicationContext(), Main14Activity.class));
+                        overridePendingTransition(0, 0);
+                        return true;
+                }
+
+                return false;
+            }
+        });
+
 
         cardView_phy.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -69,6 +104,7 @@ public class Main14Activity extends AppCompatActivity {
         inflater.inflate(R.menu.menu_1, popup.getMenu());
         popup.show();
     }
+
 
     @Override
     public void onBackPressed() {
